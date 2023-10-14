d865588f6a57e77c2c4182c48bab3761abbe074f
Jason Huang <jason5528460@gmail.com>
2023-10-14 13:10:19 +0800

    Add mapping for quickfix window

--- lua/remap.lua
+++ lua/remap.lua
@@ -12,3 +12,6 @@ vim.keymap.set("n", "<C-n>", vim.cmd.tabnext)
 
 -- "C-p" previous tab
 vim.keymap.set("n", "<C-p>", vim.cmd.tabprevious)
+
+-- "C-q" close quickfix window (e.g. reference preview)
+vim.keymap.set("n", "<C-q>", ":cclose <CR>")
